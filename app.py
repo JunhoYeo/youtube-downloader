9d232f38ff2fd286ecac21e62c3a990ab6a5a820
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2018-12-22 20:24:34 +0900

    UPDATE: auto download first args video

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2018, JunhoYeo (GPLv3)
 # https://github.com/JunhoYeo/youtube-downloader
-import youtube_dl
+import sys, youtube_dl
 from youParse import *
 
 ydl_opts = {
@@ -11,11 +11,13 @@ ydl_opts = {
     }],
 }
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    playlist = 'playlist-URL'
-    playlist = youParser(playlist)
-    playlist = playlist.crawl()
-    playlist += [
-        'video-URL'
-    ]
+    if len(sys.argv) != 2:
+        playlist = 'playlist-URL'
+        playlist = youParser(playlist)
+        playlist = playlist.crawl()
+        playlist += [
+            'video-URL'
+        ]
+    else: playlist = [sys.argv[1]]
     print(playlist)
     ydl.download(playlist)
